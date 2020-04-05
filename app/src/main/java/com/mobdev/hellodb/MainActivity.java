c06bbb59676c4d3088f5ed0835533ea978d49400
marcopicone <marco.picone@caligoo.com>
2020-04-05 21:24:01 +0200

    Removed unused imports

--- app/src/main/java/com/mobdev/hellodb/MainActivity.java
+++ app/src/main/java/com/mobdev/hellodb/MainActivity.java
@@ -1,17 +1,10 @@
 package com.mobdev.hellodb;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 /**
  * Created by Marco Picone (picone.m@gmail.com) 20/03/2020
